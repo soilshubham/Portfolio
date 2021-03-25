de30e4ae7d8b3846473df0953b999afade7a26c6
Sahil <soilshubham@gmail.com>
2021-03-25 11:05:46 +0530

    added footer and made navbar sticky

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 0;
+
+    function navbarFixed() {
+        
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        
+    }
+
+    navbarFixed();
+
+});

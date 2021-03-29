22603131c3f849d2294731df61d1f5d05ca31684
Sahil <soilshubham@gmail.com>
2021-03-29 10:10:21 +0530

    test

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 0;
+
+    function navbarFixed() {
+        
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll>0) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        
+    }
+
+    function goToTop() {
+        window.scroll({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+          });
+    }
+
+    navbarFixed();
+
+});
